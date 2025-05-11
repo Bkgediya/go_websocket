5d6b450bbfb7a5cafd22cfd51202ca6ae9a3a52d
Bhavdip Gediya <bhavdipgediya098@gmail.com>
2025-05-11 11:28:55 +0530

    reading from the buffer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go_websocket
 Websocket code implementation in go language
+
+// library
+go get golang.org/x/net/websocket
--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
 
-// type Server struct {
-// 	conns map[*websocket.Conn]bool
-// }
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new connection", ws.RemoteAddr())
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Read Error", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+		ws.Write([]byte("thank you for the msg!!!"))
+	}
+}
 
 func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
 	fmt.Println("websocket project init")
 }
